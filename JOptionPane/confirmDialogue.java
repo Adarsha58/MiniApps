582aa701680af87cc7fb6b721aea1403c34a7f1d
Adarsha58 <adarsha@ucsb.edu>
2018-12-29 00:23:56 -0800

    Application number 4

---
+++ JOptionPane/confirmDialogue.java
@@ -0,0 +1,36 @@
+
+/**
+ * small application showing the use of JOptionPane
+ *
+ * @author (Adarsha Poudel)
+ * @version (a version number or a date)
+ */
+import javax.swing.*;  
+import java.awt.event.*;  
+public class confirmDialogue extends WindowAdapter
+{
+    JFrame f;
+    JOptionPane p;
+    confirmDialogue(){
+        f= new JFrame();
+        f.addWindowListener(this);
+        f.setSize(200,200);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    }
+    /*
+     * sets the default Close Operation to EXIT_ON_CLOSE
+     */
+    public void windowClosing(WindowEvent e) {  
+        int a=JOptionPane.showConfirmDialog(f,"Are you sure?");  
+        if(a==JOptionPane.YES_OPTION){  
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        }  
+    }  
+     
+    public static void main(String[] args){
+        new confirmDialogue();
+    }
+    
+}
